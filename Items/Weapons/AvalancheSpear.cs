dd492286e829ba0ca3e5c9c9c23c5057c2c8b66e
broskiplays <broskiplaysyt@gmail.com>
2017-09-11 12:30:23 +0200

    Added Avalanche Spear

---
+++ Items/Weapons/AvalancheSpear.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Logicalty.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Logicalty.Items.Weapons
+{
+	public class AvalancheSpear : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+            DisplayName.SetDefault("Avalanche Spear");
+			Tooltip.SetDefault("It's like a Avalanche..");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 18;
+			item.useStyle = 5;
+			item.useAnimation = 18;
+			item.useTime = 24;
+			item.shootSpeed = 3.7f;
+			item.knockBack = 6.5f;
+			item.width = 32;
+			item.height = 32;
+			item.scale = 1f;
+			item.rare = 3;
+			item.value = Item.sellPrice(silver: 10);
+
+			item.melee = true;
+			item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+			item.noUseGraphic = true; // Important, it's kind of wired if people see two spears at one time. This prevents the melee animation of this item.
+			item.autoReuse = true; // Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
+
+			item.UseSound = SoundID.Item1;
+			item.shoot = mod.ProjectileType<AvalancheSpearProjectile>();
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			// Ensures no more than one spear can be thrown out, use this when using autoReuse
+			return player.ownedProjectileCounts[item.shoot] < 1; 
+		}
+	}
+}
