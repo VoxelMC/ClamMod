3896e2467ec4a301f663a251d56793b280c65431
broskiplays <broskiplaysyt@gmail.com>
2017-09-11 11:40:36 +0200

    Logicalty Namespace Change

--- Buffs/ArdiumBuff.cs
+++ Buffs/ArdiumBuff.cs
@@ -1,7 +1,7 @@
 using Terraria;
 using Terraria.ModLoader;
 
-namespace ClamMod.Buffs
+namespace Logicalty.Buffs
 {
 	public class ArdiumBuff : ModBuff
 	{
--- Buffs/ZombieMinionBuff.cs
+++ Buffs/ZombieMinionBuff.cs
@@ -1,7 +1,7 @@
 ﻿using Terraria;
 using Terraria.ModLoader;
 
-namespace ClamMod.Buffs
+namespace Logicalty.Buffs
 {
     public class ZombieMinionBuff : ModBuff
     {
